943b77dab705d5d164d5b2fc486fe7c6f10c8fb0
Drashtee <drashteeparmar@gmail.com>
2022-08-16 22:00:52 -0400

    modified main for all factors

---
+++ Challenges/All Factors/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("***************************");
+        System.out.println("Here are factors of 6");
+        System.out.println("***************************");
+        FactorPrinter.printFactors(6);
+
+        System.out.println("***************************");
+        System.out.println("Here are factors of 32");
+        System.out.println("***************************");
+        FactorPrinter.printFactors(32);
+
+        System.out.println("***************************");
+        System.out.println("Here are factors of 10");
+        System.out.println("***************************");
+        FactorPrinter.printFactors(10);
+
+        System.out.println("***************************");
+        System.out.println("Here are factors of -1");
+        System.out.println("***************************");
+        FactorPrinter.printFactors(-1);
+    }
+}
