38056cddc03138bdd43bbb2acf753111de6646ee
Drashtee <drashteeparmar@gmail.com>
2022-08-16 21:54:27 -0400

    Finding All Factors

---
+++ Challenges/GreatestCommonDivisor/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisior(12,30));
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisior(25,15));
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisior(9,18)); // the first parameter is less than 10 = false
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisior(81,153));
+
+    }
+}
