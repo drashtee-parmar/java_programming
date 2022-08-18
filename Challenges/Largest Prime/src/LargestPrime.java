fc5a878581e710c0151774d581ee97cc5ce5fe26
Drashtee <drashteeparmar@gmail.com>
2022-08-17 20:17:10 -0400

    largest prime

---
+++ Challenges/Largest Prime/src/LargestPrime.java
@@ -0,0 +1,13 @@
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+        for (int i = 2; i < number; i++)
+            if (number % i == 0) {
+                return getLargestPrime(number / i);
+            }
+        return number;
+
+    }
+}
