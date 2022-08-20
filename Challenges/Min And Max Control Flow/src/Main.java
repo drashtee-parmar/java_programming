7f8d86f4f7a2d585637ad427c2801b398fb6e018
Drashtee <drashteeparmar@gmail.com>
2022-08-20 14:05:48 -0400

    min and max using scanner

--- Challenges/Min And Max Control Flow/src/Main.java
+++ Challenges/Min And Max Control Flow/src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+//        int min = 2147483647;
+//        int max = -2147483647;
+//        int min = 0;
+//        int max = 0;
+//        boolean first = true;
+
+        while (true) {
+            System.out.println("Enter number:");
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+
+//                if (first){
+//                    first = false;
+//                    min = number;
+//                    max = number;
+//                }
+                if (number > max){
+                    max = number;
+                }
+
+                if(number < min){
+                    min = number;
+                }
+            } else {
+                break;
+            }
+            scanner.nextLine(); // handle input
+        }
+        System.out.println("min = " + min + " , max = " + max);
+
+
+        scanner.close();
     }
 }
