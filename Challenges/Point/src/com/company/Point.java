68bca6a45ca07149d4d1fdd4418bf8bd85f4b428
Drashtee <drashteeparmar@gmail.com>
2022-08-23 15:03:17 -0400

    point challenge using constructor and get/set method

---
+++ Challenges/Point/src/com/company/Point.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Point {
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return distance(0, 0);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point another) {
+        return distance(another.x, another.y);
+    }
+
+    private int x;
+    private int y;
+}
