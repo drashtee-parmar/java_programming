5d63096e1f71c5e1e7e7430304335b146fe3d19b
Drashtee <drashteeparmar@gmail.com>
2022-10-28 23:36:43 -0400

    modified grocery list

--- Practice/ListAndArrayList/src/com/company/GroceryList.java
+++ Practice/ListAndArrayList/src/com/company/GroceryList.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class GroceryList {
     private ArrayList<String> groceryList = new ArrayList<String>(); // defining new array list
 
+    /* Getter for groceryList*/
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
     /* Add to the list */
     public void addGroceryItem(String item) {
         groceryList.add(item);
@@ -21,29 +26,56 @@ public class GroceryList {
         }
     }
 
+    public void modifyGroceryItem(String currentItem, String newItem){
+        int position = findItem((currentItem));
+        if (position >= 0){
+            modifyGroceryItem(position, newItem);
+        }
+    }
+
     /* modify/update an item */
-    public void modifyGroceryItem(int position, String newItem) {
+    private void modifyGroceryItem(int position, String newItem) {
         //replace the item at that location
         groceryList.set(position, newItem);
         System.out.println("Grocery item " + (position + 1) + " has been modified.");
     }
 
     /* Remove an item */
-    public void removeGroceryItem(int position) {
-        String theItem = groceryList.get(position); // retrieve the item first
+
+    public void removeGroceryItem(String item){
+        int position = findItem((item));
+        if (position >= 0){
+            modifyGroceryItem(position, item);
+        }
+    }
+    private void removeGroceryItem(int position) {
         groceryList.remove(position);
     }
 
     /* Find the items */
-    // search for that string in our array list
-    public String findItem(String searchItem) {
-        // using contains function - to do fast search for us
-//        boolean exists = groceryList.contains(searchItem); // contains- check to see if this list contains specified element
-        //indexOf - Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
-        int position = groceryList.indexOf(searchItem);
-        if (position >= 0) {
-            return groceryList.get(position);
-        }
-        return null;
+    private int findItem(String searchItem) {
+        return groceryList.indexOf(searchItem);
     }
+    public boolean onFile(String searchItem){
+        int position = findItem(searchItem);
+        if (position >= 0){
+            return true;
+        }
+        return false;
+    }
+        /*
+        *  OR
+        * */
+
+    // search for that string in our array list
+//    public String findItem(String searchItem) {
+//        // using contains function - to do fast search for us
+////        boolean exists = groceryList.contains(searchItem); // contains- check to see if this list contains specified element
+//        //indexOf - Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+//        int position = groceryList.indexOf(searchItem);
+//        if (position >= 0) {
+//            return groceryList.get(position);
+//        }
+//        return null;
+//    }
 }
